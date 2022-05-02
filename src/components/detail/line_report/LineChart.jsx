e72d19669fb3d0a22ecd5275bb25c501e410a825
zamanbek.turukbayev <zamanbek.turukbayev@fdrive.kz>
2022-05-02 15:57:55 +0600

    Appex Line Chart

--- src/components/detail/line_report/LineChart.jsx
+++ src/components/detail/line_report/LineChart.jsx
@@ -1,62 +1,49 @@
-/* App.js */
-import React from 'react';
-import {CanvasJSChart, CanvasJS} from 'canvasjs-react-charts'
+import ReactApexChart from 'react-apexcharts'
 
+const series = [{
+    name: "Desktops",
+    data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+}]
 const options = {
-    animationEnabled: true,
-    exportEnabled: true,
-    theme: "light2", // "light1", "dark1", "dark2"
-    title:{
-        text: "Bounce Rate by Week of Year"
+    chart: {
+        height: 350,
+            type: 'line',
+            zoom: {
+            enabled: false
+        }
     },
-    axisY: {
-        title: "Bounce Rate",
-        suffix: "%"
+    dataLabels: {
+        enabled: false
     },
-    axisX: {
-        title: "Week of Year",
-        prefix: "W",
-        interval: 2
+    stroke: {
+        curve: 'straight'
     },
-    data: [{
-        type: "line",
-        toolTipContent: "Week {x}: {y}%",
-        dataPoints: [
-            { x: 1, y: 64 },
-            { x: 2, y: 61 },
-            { x: 3, y: 64 },
-            { x: 4, y: 62 },
-            { x: 5, y: 64 },
-            { x: 6, y: 60 },
-            { x: 7, y: 58 },
-            { x: 8, y: 59 },
-            { x: 9, y: 53 },
-            { x: 10, y: 54 },
-            { x: 11, y: 61 },
-            { x: 12, y: 60 },
-            { x: 13, y: 55 },
-            { x: 14, y: 60 },
-            { x: 15, y: 56 },
-            { x: 16, y: 60 },
-            { x: 17, y: 59.5 },
-            { x: 18, y: 63 },
-            { x: 19, y: 58 },
-            { x: 20, y: 54 },
-            { x: 21, y: 59 },
-            { x: 22, y: 64 },
-            { x: 23, y: 59 }
-        ]
-    }]
+    title: {
+        text: 'Robot Requests',
+            align: 'left'
+    },
+    grid: {
+        row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+        },
+    },
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+    }
 }
+
 
 function LineChart () {
     return (
-        <div>
-            <CanvasJSChart options = {options}
-                /* onRef={ref => this.chart = ref} */
-            />
-            {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+
+        <div id="chart">
+            <ReactApexChart options={options} series={series} type="line" height={500} />
         </div>
+
+
     );
 }
+
 export default LineChart
+    
